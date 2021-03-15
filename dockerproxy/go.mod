443fd77847ad1a91318e4de249d38e37842db3d7
Michael Dwan <m@dwan.io>
2021-03-15 14:18:48 -0600

    replace ssh proxy with http reverse proxy

--- dockerproxy/go.mod
+++ dockerproxy/go.mod
@@ -3,13 +3,11 @@ module github.com/superfly/rchab/dockerproxy
 go 1.16
 
 require (
-	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
-	github.com/gliderlabs/ssh v0.3.2
+	github.com/gorilla/handlers v1.5.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.7.0
 	github.com/superfly/flyctl v0.0.163
-	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
 
 replace github.com/containerd/containerd => github.com/containerd/containerd v1.3.1-0.20200227195959-4d242818bf55
--- dockerproxy/main.go
+++ dockerproxy/main.go
@@ -1,34 +1,24 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/base64"
 	"encoding/json"
-	"encoding/pem"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"os/exec"
 	"os/signal"
-	"path/filepath"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
 
-	"github.com/gliderlabs/ssh"
+	"github.com/gorilla/handlers"
 	"github.com/patrickmn/go-cache"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/superfly/flyctl/api"
-
-	gossh "golang.org/x/crypto/ssh"
 )
 
 type ctxKey string
@@ -79,14 +69,10 @@ func main() {
 
 	httpServer := &http.Server{
 		Addr:    ":8080",
-		Handler: basicAuth(verifyApp(proxy())),
+		Handler: handlers.LoggingHandler(log.Writer(), authRequest(resetDeadline(proxy()))),
 
 		// reuse the context we've created
 		BaseContext: func(_ net.Listener) context.Context { return ctx },
-
-		// kosher timeouts
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
 	}
 
 	// Run server
@@ -95,39 +81,6 @@ func main() {
 		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
 			// it is fine to use Fatal here because it is not main gorutine
 			log.Fatalf("HTTP server ListenAndServe: %v", err)
-		}
-	}()
-
-	privateKeyPath := "id_rsa"
-	if dataDir, ok := os.LookupEnv("DATA_DIR"); ok {
-		privateKeyPath = filepath.Join(dataDir, privateKeyPath)
-	}
-
-	pk, err := loadPrivateKey(privateKeyPath)
-	if err != nil {
-		log.Println("Error loading private key, generating new keys", err)
-		pk, err = generatePrivateKey(privateKeyPath)
-		if err != nil {
-			log.Fatalln("Failed to generate private key", err)
-		}
-	}
-
-	signer, err := gossh.NewSignerFromKey(pk)
-
-	sshServer := &ssh.Server{
-		Addr:        ":2222",
-		Handler:     sshHandler,
-		HostSigners: []ssh.Signer{signer},
-		PasswordHandler: func(ctx ssh.Context, password string) bool {
-			return authorizeRequestWithCache(ctx.User(), password)
-		},
-	}
-
-	// Run server
-	go func() {
-		log.Infoln("ssh proxy listening on", sshServer.Addr)
-		if err := sshServer.ListenAndServe(); err != ssh.ErrServerClosed {
-			log.Fatalln("ssh server ListenAndServe", err)
 		}
 	}()
 
@@ -171,7 +124,7 @@ ALIVE:
 	gracefullCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelShutdown()
 
-	if err := sshServer.Shutdown(gracefullCtx); err != nil {
+	if err := httpServer.Shutdown(gracefullCtx); err != nil {
 		log.Warnf("shutdown error: %v\n", err)
 		defer os.Exit(1)
 		return
@@ -244,199 +197,70 @@ func runDockerd() (func(), error) {
 	return stopFn, nil
 }
 
-// get app name and access token from Proxy-Authorization
-// set them on the request context for later use
-func basicAuth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Debug("basicAuth: called")
-		proxyauth := r.Header.Get("Proxy-Authorization")
-		if proxyauth == "" {
-			w.WriteHeader(http.StatusProxyAuthRequired)
-			return
-		}
+// proxy to docker sock, by hijacking the connection
+func proxy() http.Handler {
+	proxy := &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme = "http"
+			r.URL.Host = "localhost"
+			fmt.Println(r.URL)
+		},
+		Transport: &http.Transport{
+			Dial: func(network, addr string) (net.Conn, error) {
+				fmt.Println("dial", network, addr)
+				return net.Dial("unix", "/var/run/docker.sock")
+			},
+		},
+	}
 
-		appName, accessToken, ok := parseBasicAuth(proxyauth)
-		if !ok {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			return
-		}
-
-		log.Debugf("basicAuth: app=%s", appName)
-
-		ctx := context.WithValue(context.WithValue(r.Context(), appNameKey, appName), accessTokenKey, accessToken)
-
-		log.Debug("basicAuth: calling next")
-
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+	return proxy
 }
 
-// retrieve app and validate org is allowed
-func verifyApp(next http.Handler) http.Handler {
+func authRequest(next http.Handler) http.Handler {
+	if noAuth {
+		return next
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Debug("verifyApp: called")
-		appName, ok := r.Context().Value(appNameKey).(string)
-		if !ok {
-			log.Error("something is seriously wrong, couldn't get appName")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		accessToken, ok := r.Context().Value(accessTokenKey).(string)
-		if !ok {
-			log.Error("something is seriously wrong, couldn't get accessToken")
-			w.WriteHeader(http.StatusInternalServerError)
+		appName, authToken, ok := r.BasicAuth()
+
+		if !ok || !authorizeRequestWithCache(appName, authToken) {
+			if err := writeDockerDaemonResponse2(w, http.StatusUnauthorized, "You are not authorized to use this builder"); err != nil {
+				log.Warnln("error writing response", err)
+			}
 			return
 		}
 
-		api.SetBaseURL("https://api.fly.io")
-		fly := api.NewClient(accessToken, "0.0.0.0.0.0.1")
-		app, err := fly.GetApp(appName)
-		if app == nil || err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			_, _ = w.Write([]byte("App not found"))
-			return
-		}
-
-		org, err := fly.FindOrganizationBySlug(orgSlug)
-		if org == nil || err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			_, _ = w.Write([]byte("Organization not found"))
-			return
-		}
-
-		if app.Organization.ID != org.ID {
-			w.WriteHeader(http.StatusUnauthorized)
-			_, _ = w.Write([]byte("Wrong organization"))
-			return
-		}
-
-		log.Debug("verifyApp: calling next")
 		next.ServeHTTP(w, r)
 	})
 }
 
-// proxy to docker sock, by hijacking the connection
-func proxy() http.Handler {
+func resetDeadline(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Debug("proxy: called")
-		defer log.Debug("proxy: done")
+		buildsWg.Add(1)
+		defer buildsWg.Done()
+
 		defer func() {
 			log.Debug("resetting deadline")
 			jobDeadline.Reset(maxIdleDuration)
 		}()
 
-		target := "/var/run/docker.sock"
-
-		var c net.Conn
-
-		cl, err := net.Dial("unix", target)
-		if err != nil {
-			log.Errorf("error connecting to backend: %s", err)
-			return
-		}
-
-		c = cl
-		hj, ok := w.(http.Hijacker)
-		if !ok {
-			http.Error(w, "hijack error", 500)
-			return
-		}
-		nc, _, err := hj.Hijack()
-		if err != nil {
-			log.Infof("hijack error: %v", err)
-			return
-		}
-
-		buildsWg.Add(1)
-		defer buildsWg.Done()
-
-		defer nc.Close()
-		defer c.Close()
-
-		err = r.Write(c)
-		if err != nil {
-			log.Infof("error copying request to target: %v", err)
-			return
-		}
-
-		errc := make(chan error, 2)
-		cp := func(dst io.Writer, src io.Reader) {
-			_, err := io.Copy(dst, src)
-			errc <- err
-		}
-		go cp(c, nc)
-		go cp(nc, c)
-		<-errc
+		next.ServeHTTP(w, r)
 	})
 }
 
-func sshHandler(s ssh.Session) {
-	log.Infoln("starting session for", s.RemoteAddr())
-
-	defer func() {
-		log.Debug("resetting deadline")
-		jobDeadline.Reset(maxIdleDuration)
-	}()
-
-	dockerConn, err := net.Dial("unix", "/var/run/docker.sock")
-	if err != nil {
-		log.Errorln("error connecting to docker daemon:", err)
-		writeDockerDaemonResponse(s, http.StatusInternalServerError, errors.Wrap(err, "Unable to connect to the docker daemon").Error())
-		return
-	}
-	log.Infoln("connected to docker")
-
-	buildsWg.Add(1)
-	defer buildsWg.Done()
-
-	defer dockerConn.Close()
-
-	done := make(chan error)
-
-	// client -> docker
-	go func() {
-		sock := dockerConn.(*net.UnixConn)
-		n, err := io.Copy(sock, s)
-		log.Debugf("%s client->docker: copied %d bytes: %v", s.RemoteAddr(), n, err)
-		sock.CloseWrite()
-	}()
-
-	// docker -> client
-	go func() {
-		n, err := io.Copy(s, dockerConn)
-		log.Debugf("%s client<-docker: copied %d bytes: %v", s.RemoteAddr(), n, err)
-		s.CloseWrite()
-
-		done <- err
-	}()
-
-	log.Infoln("waiting for session to end", s.RemoteAddr())
-
-	if err := <-done; err != nil {
-		log.Warnln("Error writing to client", err)
-	}
-
-	log.Infoln("finished session", s.RemoteAddr())
-}
-
-func writeDockerDaemonResponse(w io.Writer, status int, message string) error {
-	var body bytes.Buffer
-	json.NewEncoder(&body).Encode(map[string]string{"message": message})
-
-	r := http.Response{
-		StatusCode: status,
-		Body:       io.NopCloser(&body),
-		Header: map[string][]string{
-			"Content-Type": {"application/json"},
-		},
-	}
-
-	return r.Write(w)
+func writeDockerDaemonResponse2(w http.ResponseWriter, status int, message string) error {
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(map[string]string{"message": message})
 }
 
 func authorizeRequestWithCache(appName, authToken string) bool {
 	if noAuth {
 		return true
+	}
+
+	if appName == "" || authToken == "" {
+		return false
 	}
 
 	cacheKey := appName + ":" + authToken
@@ -473,63 +297,4 @@ func authorizeRequest(appName, authToken string) bool {
 	}
 
 	return true
-}
-
-func parseBasicAuth(auth string) (username, password string, ok bool) {
-	auth = strings.TrimPrefix(strings.TrimPrefix(auth, "Basic "), "basic ")
-	c, err := base64.StdEncoding.DecodeString(auth)
-	if err != nil {
-		log.Warnln("Error decoding auth", err)
-		return
-	}
-	cs := string(c)
-	s := strings.IndexByte(cs, ':')
-	if s < 0 {
-		return
-	}
-	return cs[:s], cs[s+1:], true
-}
-
-func loadPrivateKey(filename string) (*rsa.PrivateKey, error) {
-	privateKeyBytes, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	block, _ := pem.Decode(privateKeyBytes)
-	if block == nil || block.Type != "RSA PRIVATE KEY" {
-		return nil, fmt.Errorf("Expected \"RSA PRIVATE KEY\" found \"%s\"", block.Type)
-	}
-
-	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, errors.Wrap(err, "Errror parsing private key")
-	}
-
-	return privateKey, nil
-}
-
-func generatePrivateKey(filename string) (*rsa.PrivateKey, error) {
-	// generate key
-	privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return nil, errors.Wrap(err, "Cannot generate RSA key")
-	}
-
-	// dump private key to file
-	var privateKeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
-	privateKeyBlock := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: privateKeyBytes,
-	}
-	privatePem, err := os.Create(filename)
-	if err != nil {
-		return nil, errors.Wrap(err, "error creating private.pem")
-	}
-	err = pem.Encode(privatePem, privateKeyBlock)
-	if err != nil {
-		return nil, errors.Wrap(err, "error encoding private pem")
-	}
-
-	return privatekey, nil
 }
